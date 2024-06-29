98140a999a882847db83e723d0a779a49c34b003
munim khaliq <97252132+munim-k@users.noreply.github.com>
2024-06-29 18:45:07 +0500

    day 2

---
+++ Assets/scripts/networkManagerUI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class networkManagerUI : MonoBehaviour
+{
+   [serializeField] private Button svrButton;
+   [serializeField] private Button hostButton;
+   [serializeField] private Button clientButton;
+
+
+   private void awake()
+   {
+    
+   }
+}
